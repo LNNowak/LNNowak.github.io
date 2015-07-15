84ac9b6be403d84aaf53aafffb014289ef3873c5
LNNowak <lnowak4@mion.elka.pw.edu.pl>
2015-07-15 19:12:27 +0200

    Commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,6 @@
 $(document).ready(function(){
   $('div#mainDiv').append('<p>New, jQuery paragraph!</p>');
+  $('div#thirdDiv').click(function() {
+    $(this).animate({marginTop: '+=5%'}, 'slow');
+  });
 });
